265ac3a910da34b44a320ba9f96c2f8d1d65798f
OliviaTheVampire <sindrefagerheim53@gmail.com>
2020-11-04 07:42:17 +0100

    testing

--- src/main/java/io/github/vampirestudios/raa_dimension/utils/Testing.java
+++ src/main/java/io/github/vampirestudios/raa_dimension/utils/Testing.java
@@ -1,5 +1,6 @@
 package io.github.vampirestudios.raa_dimension.utils;
 
+import io.github.vampirestudios.raa_dimension.RAADimensionAddon;
 import io.github.vampirestudios.raa_dimension.init.Dimensions;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.util.math.MathHelper;
@@ -9,7 +10,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class Testing {
 
     public static double changeGravity(World world) {
-        if (world.getRegistryKey().getValue().getNamespace().equals("raa_dimensions")) {
+        System.out.println(world.getRegistryKey().getValue());
+        if (world.getRegistryKey().getValue().getNamespace().equals(RAADimensionAddon.MOD_ID)) {
             return Dimensions.DIMENSIONS.get(world.getRegistryKey().getValue()).getGravity() * 0.08d;
         }
         return 0.08d;
@@ -17,7 +19,8 @@ public class Testing {
 
     public static void changeGravityDamage(StatusEffectInstance statusEffectInstance, World world, float fallDistance, float damageMultiplier, CallbackInfoReturnable<Integer> info) {
         float f = statusEffectInstance == null ? 0.0F : (float)(statusEffectInstance.getAmplifier() + 1);
-        if (world.getRegistryKey().getValue().getNamespace().equals("raa_dimensions")) {
+        System.out.println(world.getRegistryKey().getValue());
+        if (world.getRegistryKey().getValue().getNamespace().equals(RAADimensionAddon.MOD_ID)) {
             info.setReturnValue((int) (MathHelper.ceil((fallDistance - 3.0F - f) * damageMultiplier) * Dimensions.DIMENSIONS.get(world.getRegistryKey().getValue()).getGravity()));
         }
     }
